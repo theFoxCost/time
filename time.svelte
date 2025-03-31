6cc85cdc2cf07d77acaefd606e7b7e2f541c2700
theFoxCost <extractiongames@gmail.com>
2025-03-31 13:13:12 +0100

    time is MONEY...what i'm talking about

---
+++ time.svelte
@@ -0,0 +1,23 @@
+<script>
+    import moment from "moment"; 
+    import { onMount } from "svelte";
+  
+    let ctime = moment().format("YYYY-MM-DD HH:mm:ss");
+  
+    onMount(() => {
+      setInterval(() => {
+        ctime = moment().format("YYYY-MM-DD HH:mm:ss");
+      }, 1000);
+    });
+  </script>
+  
+  <main>
+    <h1>{ctime}</h1>
+  </main>
+  
+  <style>
+    h1 {
+      color: black;
+    }
+  </style>
+  
